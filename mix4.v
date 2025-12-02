b844a3bd312158e3d5be3435e42ddb501641655b
Matthew Morales <mtthwmrls9@gmail.com>
2025-12-01 22:23:37 -0700

    Attempt triangle wave layering for noise gen

--- mix4.v
+++ mix4.v
@@ -1,8 +1,9 @@
 module mix4 (
     input wire [7:0] samp0, samp1, samp2, samp3,
-    output wire [15:0] samp_out
+    output reg [15:0] samp_out
 );
-
-    assign samp_out = {8'd0, samp0} + {8'd0, samp1} + {8'd0, samp2} + {8'd0, samp3};
+    always @(*) begin
+        samp_out = {8'd0, samp0} + {8'd0, samp1} + {8'd0, samp2} + {8'd0, samp3};
+    end
 
 endmodule
--- noise_gen.v
+++ noise_gen.v
@@ -1,17 +1,47 @@
 module noise_gen (
 	input wire clk, reset,
     input wire [31:0] period,
-	output wire [7:0] value
+	output reg [7:0] value
 );
 
+    wire [7:0] tri_out0, tri_out1, tri_out2, tri_out3;
     wire [15:0] rng_out;
 
-    lsfr rng (
+    triangle_wave_gen twg0 (
         .clk(clk),
         .reset(reset),
-        .data(rng_out)
+        .period(period),
+        .value(tri_out0)
     );
 
-    assign value = period ? {2'd0, rng_out[5:0]} : 8'd0;
+    triangle_wave_gen twg1 (
+        .clk(clk),
+        .reset(reset),
+        .period(period >> 1),
+        .value(tri_out1)
+    );
+
+    triangle_wave_gen twg2 (
+        .clk(clk),
+        .reset(reset),
+        .period(period >> 2),
+        .value(tri_out2)
+    );
+
+    triangle_wave_gen twg3 (
+        .clk(clk),
+        .reset(reset),
+        .period(period << 1),
+        .value(tri_out3)
+    );
+
+    always @(*) begin
+        value = (tri_out0 >> 2) + (tri_out1 >> 2) + (tri_out2 >> 2) + (tri_out3 >> 2);
+        // if (period !== 32'd0) begin
+        //     value = {4'd0, rng_out[3:0]} + {tri_out >> 1};
+        // end else begin
+        //     value = 4'd0;
+        // end
+    end
 
 endmodule
